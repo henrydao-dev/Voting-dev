4f4fa62d30aab621a73b3054845de0a8536574a0
dkiang <douglas.kiang@gmail.com>
2021-06-20 07:24:06 -1000

    Created Canddiate.java

---
+++ Candidate.java
@@ -0,0 +1,48 @@
+// Candidate.java
+// Represents a Candidate for a particular office
+public class Candidate
+{
+    /*
+    private String name
+Candidate's name (used only in toString)
+
+
+private int candidateID
+unique ID for candidate
+Used by voters. txt, so should not change
+private String office
+office the candidate is running for
+Could use an enum
+private int votes
+How many votes candidate got
+
+
+public Candidate(first, last, office)
+initialize votes to zero
+
+
+public String getFirstName
+
+
+
+
+public String getLastName
+
+
+
+
+public String getOffice
+
+
+
+
+public String toString
+Outputs a text-friendly string
+"Gavin Newsom, running for Governor"
+
+    
+    
+    
+    
+    /*
+}
